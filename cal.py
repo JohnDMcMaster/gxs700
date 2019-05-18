ccc828d78a439466e4a0e3633b89704a414e5acc
John McMaster <johndmcmaster@gmail.com>
2019-05-18 10:32:11 -0700

    cal: support directory input
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- cal.py
+++ cal.py
@@ -5,21 +5,24 @@ import numpy as np
 import glob
 import os
 
+
 def dir2np(d):
     np_df2s = []
     for fn in glob.glob(d + '/*.png'):
         np_df2s.append(np.array(Image.open(fn)))
     return np.average(np_df2s)
 
+
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description='Correct images against darkfield and flat field capture')
+    parser = argparse.ArgumentParser(
+        description='Correct images against darkfield and flat field capture')
     parser.add_argument('--verbose', '-v', action='store_true', help='verbose')
     parser.add_argument('df_png', help='Dark field .png ("force capture")')
     parser.add_argument('ff_png', help='Flat field .png ("no sample")')
     parser.add_argument('din', help='Input directory or file')
-    parser.add_argument('dout', help='Output directory or file')
+    parser.add_argument('dout', nargs='?', help='Output directory or file')
 
     args = parser.parse_args()
 
@@ -47,10 +50,7 @@ if __name__ == "__main__":
     cal_det = np.maximum(cal_det, u16_ones)
     cal_scalar = 0xFFFF / cal_det
 
-    if not os.path.exists(args.dout):
-        os.mkdir(args.dout)
-
-    for fn_in in glob.glob(args.din + '/*.png'):
+    def process(fn_in, fn_out):
         print 'Processing %s' % fn_in
         im_in = Image.open(fn_in)
         np_in2 = np.array(im_in)
@@ -59,4 +59,21 @@ if __name__ == "__main__":
         np_scaled = np.minimum(np_scaled, u16_maxs)
         np_scaled = np.maximum(np_scaled, u16_mins)
         imc = Image.fromarray(np_scaled).convert("I")
-        imc.save(os.path.join(args.dout, os.path.basename(fn_in)))
+        imc.save(fn_out)
+
+    if os.path.isdir(args.din):
+        if not os.path.exists(args.dout):
+            os.mkdir(args.dout)
+
+        for fn_in in glob.glob(args.din + '/*.png'):
+            fn_out = os.path.join(args.dout, os.path.basename(fn_in))
+            process(fn_in, fn_out)
+    elif os.path.isfile(args.din):
+        fn_in = args.din
+        fn_out = args.dout
+        if not fn_out:
+            fn_out = fn_in.replace('.png', '_cal.png')
+            assert fn_in != fn_out
+        process(fn_in, fn_out)
+    else:
+        raise Exception("Bad input file/dir")
--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ import os
 from setuptools import setup
 import shutil
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 if not os.path.exists('build'):
     os.mkdir('build')
@@ -19,9 +21,10 @@ scripts = (
     'hist_eq_dir.py',
     'img2bin.py',
     'mask.py',
+    'cal.py',
     #'prog_eeprom.py',
     #'restore_dev.py',
-    )
+)
 scripts_dist = []
 for script in scripts:
     # Make script names more executable like
@@ -30,13 +33,13 @@ for script in scripts:
     scripts_dist.append(dst)
 
 setup(
-    name = "gxs700",
-    version = "1.0.0",
-    author = "John McMaster",
+    name="gxs700",
+    version="1.0.0",
+    author="John McMaster",
     author_email='JohnDMcMaster@gmail.com',
-    description = ("Gendex GXS700 / Dexis Platinum USB x-ray sensor driver."),
-    license = "BSD",
-    keywords = "gxs700 gendex dexis x-ray",
+    description=("Gendex GXS700 / Dexis Platinum USB x-ray sensor driver."),
+    license="BSD",
+    keywords="gxs700 gendex dexis x-ray",
     url='https://github.com/JohnDMcMaster/gxs700',
     packages=['gxs700'],
     scripts=scripts_dist,
